8ad741a5b74a8b6ff9c8171c73a170e6c5010e4c
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-10-25 18:46:32 +0900

    feat: gen-openapi

--- gen-openapi.sh
+++ gen-openapi.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
+# npm i -g swagger-combine
+set -eo pipefail
 
 rm swagger.yaml
-cp ~/src/github.com/CosmWasm/wasmd/docs/client/swagger-ui/swagger.yaml ./swagger.yaml
+
+# Copy proto
+cp -r ~/github.com/CosmWasm/wasmd/proto ./proto
+cp -r ~/github.com/CosmWasm/wasmd/third_party/proto ./proto-thirdparty
+
+mkdir -p ./tmp-swagger-gen
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+
+  # generate swagger files (filter query files)
+  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+  if [[ ! -z "$query_file" ]]; then
+    buf protoc  \
+    -I "proto" \
+    -I "third_party/proto" \
+    "$query_file" \
+    --swagger_out=./ \
+    --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
+  fi
+done
+
+# clean swagger files
+rm -r ./proto
+rm -r ./proto-thirdparty
 
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
