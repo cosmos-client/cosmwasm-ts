18df2dc63b3ef4d61b5329b009b905221c710615
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-10-27 16:02:16 +0900

    feat: openapi package

--- gen-openapi.sh
+++ gen-openapi.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # npm i -g swagger-combine
-set -eo pipefail
+# set -eo pipefail
 
-rm swagger.yaml
+rm -r swagger.yaml
+rm -r ./proto
+rm -r ./proto-thirdparty
 
 # Copy proto
 cp -r ~/github.com/CosmWasm/wasmd/proto ./proto
@@ -17,17 +19,26 @@ for dir in $proto_dirs; do
   if [[ ! -z "$query_file" ]]; then
     buf protoc  \
     -I "proto" \
-    -I "third_party/proto" \
+    -I "proto-thirdparty" \
     "$query_file" \
-    --swagger_out=./ \
+    --swagger_out=./tmp-swagger-gen \
     --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
   fi
 done
 
+# combine swagger files
+# uses nodejs package `swagger-combine`.
+# all the individual swagger files need to be configured in `config.json` for merging
+swagger-combine ./configs/config.json -o ./tmp-swagger-gen/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+
+cp ./tmp-swagger-gen/swagger.yaml ./swagger.yaml
+
 # clean swagger files
 rm -r ./proto
 rm -r ./proto-thirdparty
+rm -r ./tmp-swagger-gen
 
+# generate openapi
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -g typescript-axios -i /local/swagger.yaml -o /local/src/openapi/
